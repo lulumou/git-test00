6e8a47c1269b47b8d45d69f03ab16534f67baa97
lumou <997447204@qq.com>
2022-08-01 16:59:16 +0800

    second commit

--- src/main/java/com/lumou/git/GitTest.java
+++ src/main/java/com/lumou/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lumou.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
